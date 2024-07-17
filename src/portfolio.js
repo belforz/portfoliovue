2c27d12efe683e7ff109effe2bb6a3f24271728c
Leo <macedobeiramar@hotmail.com>
2024-07-16 21:35:04 -0300

    fix:path-curriculum

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     name: "Leandro Belfor",
     message: "Sou estudande de Análise e Desenvolvimento de Sistemas, cai de paraquedas nesse mundo digital e olha que até que meu pouso foi bem, que tal conhecer-lo?",
     basedLocation: "São Paulo, SP",
-    resumeLink: "https://drive.google.com/file/d/1D8KHoa35TwvZGihWJpsQaImqdp8a1rS4/view?usp=sharing", // recommended: google drive file share link (change to "anyone on the internet can view")
+    resumeLink: "https://drive.google.com/file/d/1y9HqXRjA1JSK9Orce1wNlPfQNfynjHO6/view?usp=sharing", // recommended: google drive file share link (change to "anyone on the internet can view")
     logo: {
         // link: "images/logo1.png", // use relative path from  parent directory -> ex: images/image.ext
         custom: true // takes precedence over image logo and allows for custom HTML logo (./components/Navbar.vue)
